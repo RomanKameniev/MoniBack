4340e85b0e0e9b9b3c45e811153ee06cf6de6aaf
Roman Kameniev <roman.kameniev@gmail.com>
2020-04-19 14:47:49 +0300

    -added logger and 404

--- app/src/app.js
+++ app/src/app.js
@@ -1,24 +1,49 @@
 import Koa from 'koa'
 
-//import route from 'koa-route'
 import Router from 'koa-router'
-
-//import websockify from 'koa-websocket'
-import logger from 'koa-logger'
+import loggerL from 'koa-logger'
+//import logger from 'koa-morgan'
 const app = new Koa()
-//const app = websockify(new Koa())
 var router = new Router()
-app.use(logger())
 
-router.get('/', (ctx, next) => {
-	console.log('ctx => ', ctx)
-	return next()
+//app.use(logger('combined'))
+app.use(loggerL())
+
+router.get('/', (ctx) => {
+	ctx.body = 'I am root!'
+})
+
+router.get('/second_route', (ctx) => {
+	ctx.body = 'I am second_route'
+})
+
+router.post('/something', (ctx) => {
+	ctx.body = {
+		something: 'something here',
+	}
+})
+
+app.use(async function pageNotFound(ctx) {
+	// we need to explicitly set 404 here
+	// so that koa doesn't assign 200 on body=
+	ctx.status = 404
+
+	switch (ctx.accepts('html', 'json')) {
+		case 'html':
+			ctx.type = 'html'
+			ctx.body = '<p>Page Not Found</p>'
+			break
+		case 'json':
+			ctx.body = {
+				message: 'Page Not Found',
+			}
+			break
+		default:
+			ctx.type = 'text'
+			ctx.body = 'Page Not Found'
+	}
 })
 
 app.use(router.routes()).use(router.allowedMethods())
-// Regular middleware
-// Note it's app.ws.use and not app.use
-
-// Note it's app.ws.use and not app.use
 
 app.listen(3001)
