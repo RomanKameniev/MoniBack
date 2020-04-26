294b6ae45cff9a22073b7a36f1c27887e0bebf29
Roman Kameniev <roman.kameniev@gmail.com>
2020-04-27 02:21:19 +0300

    -added default routes

--- app/src/app.js
+++ app/src/app.js
@@ -7,13 +7,13 @@ import bodyParser from 'koa-bodyparser' // парсер для POST запрос
 import serve from 'koa-static' // модуль, который отдает статические файлы типа index.html из заданной директории
 import passport from 'koa-passport' //реализация passport для Koa
 
-
-
-// import LocalStrategy from 'passport-local' //локальная стратегия авторизации
-// import { Strategy, ExtractJwt } from 'passport-jwt' // авторизация через JWT
-// import socketioJwt from 'socketio-jwt'
-// import socketIO from 'socket.io'
-// import crypto from 'crypto'
+import LocalStrategy from 'passport-local' //локальная стратегия авторизации
+import { Strategy, ExtractJwt } from 'passport-jwt' // авторизация через JWT
+import socketioJwt from 'socketio-jwt'
+import  jwt  from 'jsonwebtoken'
+import socketIO from 'socket.io'
+import crypto from 'crypto'
+const jwtsecret = 'mysecretkey'
 
 const app = new Koa()
 var router = new Router()
@@ -25,43 +25,43 @@ app.use(serve('public'))
 app.use(loggerL())
 app.use(bodyParser())
 
+// app.use(async function pageNotFound(ctx) {
+// 	// we need to explicitly set 404 here
+// 	// so that koa doesn't assign 200 on body=
+// 	ctx.status = 404
 
-app.use(async function pageNotFound(ctx) {
-	// we need to explicitly set 404 here
-	// so that koa doesn't assign 200 on body=
-	ctx.status = 404
-
-	switch (ctx.accepts('html', 'json')) {
-		case 'html':
-			ctx.type = 'html'
-			ctx.body = '<p>Page Not Found</p>'
-			break
-		case 'json':
-			ctx.body = {
-				message: 'Page Not Found',
-			}
-			break
-		default:
-			ctx.type = 'text'
-			ctx.body = 'Page Not Found'
-	}
-})
-
+// 	switch (ctx.accepts('html', 'json')) {
+// 		case 'html':
+// 			ctx.type = 'html'
+// 			ctx.body = '<p>Page Not Found</p>'
+// 			break
+// 		case 'json':
+// 			ctx.body = {
+// 				message: 'Page Not Found',
+// 			}
+// 			break
+// 		default:
+// 			ctx.type = 'text'
+// 			ctx.body = 'Page Not Found'
+// 	}
+// })
 
 router.get('/', (ctx) => {
+	ctx.status = 200
 	ctx.body = 'I am root!'
 })
 
 router.get('/second_route', (ctx) => {
 	ctx.body = 'I am second_route'
+	ctx.status = 200
 })
+
 
 router.post('/something', (ctx) => {
 	ctx.body = {
 		something: 'something here',
 	}
 })
-
 
 app.use(router.routes()).use(router.allowedMethods())
 
